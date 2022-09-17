277d160fad68a76ac04418add6a1a47700b23507
tophe <topherclay.chris@gmail.com>
2022-09-16 18:20:27 -0700

    moving response to function so i can override the user to function

--- main.py
+++ main.py
@@ -32,15 +32,22 @@ async def on_message(message):
 
 
     if "Daily Duotrigordle" in message.content:
-        reply = await parsing_stuff.main_parse(message.content)
-        game = await stuff_to_be_saved.make_class(message.content)
-        game.user = str(message.author)
-        print(str(game))
-        await message.channel.send(reply)
-        await message.channel.send(str(game))
-        commit_result = await sql_stuff.commit_game_to_db(game)
-        await message.channel.send(commit_result)
+        user = message.author
+        score_string = message.content
+        await respond_to_score_post(user, score_string, message)
 
+
+
+
+async def respond_to_score_post(user, score_string, message):
+    reply = await parsing_stuff.main_parse(score_string)
+    game = await stuff_to_be_saved.make_class(score_string)
+    game.user = user
+    print(str(game))
+    await message.channel.send(reply)
+    await message.channel.send(str(game))
+    commit_result = await sql_stuff.commit_game_to_db(game)
+    await message.channel.send(commit_result)
 
 
 client.run(client_token)

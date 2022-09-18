ccbfe461963f0f56e1890836f1e8e5a308002370
tophe <topherclay.chris@gmail.com>
2022-09-17 19:13:56 -0700

    put it in ticks

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ async def on_message(message):
 
     if message.content == "!today":
         result = await sql_stuff.get_all_of_a_day()
+        result = "```" + result + "```"
         await message.channel.send(result)
 
 

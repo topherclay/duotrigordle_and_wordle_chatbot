a6ddd1489ea3f5deda7e274ac356afec2a35da9b
tophe <topherclay.chris@gmail.com>
2022-09-29 11:59:58 -0700

    added robot face to help message.

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ def generate_help_message():
         return f"```\n{msg}\n```"
 
     # these have to be very short to avoid wraparound.
-    message = "Here are the available commands.\n"
+    message = "🤖Here are the available commands.🤖\n"
     message += f' {COMMAND_STRING["top by rank"]:<7}: top ten sorted by turns.\n'
     message += f' {COMMAND_STRING["top by speed"]:<7}: top ten sorted by speed.\n'
     message += f' {COMMAND_STRING["show current day"]:<7}: current day\'s board.\n'

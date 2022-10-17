407fae56280c83edfaf79e7779adeb8d31ea8a1a
tophe <topherclay.chris@gmail.com>
2022-10-17 09:20:14 -0700

    changed back

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ COMMAND_STRING = {
     "show commands": "!help",
     "show top with offset": "!topfrom",
     "show speed with offset": "!speedfrom",
-    "show stats": "!statsme"
+    "show stats": "!statme"
 }
 
 
